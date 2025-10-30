843107a0df6c079259da5a1a86659cf8b02d8c21
karmat314 <karmat314@gmail.com>
2025-10-30 14:36:51 +0600

    disable cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_cable.enabled = false
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
